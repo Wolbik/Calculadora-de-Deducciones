1c9bbdb614ce322edd43b876e4bbd0fa3cd22bfe
Wolbik <102483349+Wolbik@users.noreply.github.com>
2022-05-11 21:55:45 -0400

    Add files via upload

---
+++ WindowsFormAppSencilla1/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace WindowsFormAppSencilla1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sueldo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.totalSueldoNeto = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.valorAFP = new System.Windows.Forms.Panel();
+            this.deduccionAFP = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.valorISR = new System.Windows.Forms.Panel();
+            this.deduccionISR = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.deduccionSFS = new System.Windows.Forms.Label();
+            this.valorSFS = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.valorAFP.SuspendLayout();
+            this.valorISR.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Sueldo
+            // 
+            this.Sueldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sueldo.Location = new System.Drawing.Point(18, 102);
+            this.Sueldo.Name = "Sueldo";
+            this.Sueldo.Size = new System.Drawing.Size(147, 26);
+            this.Sueldo.TabIndex = 0;
+            this.Sueldo.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sueldo Bruto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(14, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(322, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Calculadora de Deducciones";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(184, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightCoral;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(47, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ISR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightCoral;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(204, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "AFP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.LightCoral;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(361, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "SFS";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.valorAFP);
+            this.panel1.Controls.Add(this.valorISR);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.ForeColor = System.Drawing.Color.Coral;
+            this.panel1.Location = new System.Drawing.Point(20, 159);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(475, 184);
+            this.panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.totalSueldoNeto);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Location = new System.Drawing.Point(140, 142);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(124, 20);
+            this.panel2.TabIndex = 21;
+            // 
+            // totalSueldoNeto
+            // 
+            this.totalSueldoNeto.AutoSize = true;
+            this.totalSueldoNeto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalSueldoNeto.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.totalSueldoNeto.Location = new System.Drawing.Point(3, 0);
+            this.totalSueldoNeto.Name = "totalSueldoNeto";
+            this.totalSueldoNeto.Size = new System.Drawing.Size(27, 20);
+            this.totalSueldoNeto.TabIndex = 23;
+            this.totalSueldoNeto.Text = "00";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label10.Location = new System.Drawing.Point(3, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 20);
+            this.label10.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(3, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 20);
+            this.label9.TabIndex = 0;
+            // 
+            // valorAFP
+            // 
+            this.valorAFP.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.valorAFP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.valorAFP.Controls.Add(this.deduccionAFP);
+            this.valorAFP.Controls.Add(this.label8);
+            this.valorAFP.Location = new System.Drawing.Point(163, 72);
+            this.valorAFP.Name = "valorAFP";
+            this.valorAFP.Size = new System.Drawing.Size(124, 20);
+            this.valorAFP.TabIndex = 18;
+            // 
+            // deduccionAFP
+            // 
+            this.deduccionAFP.AutoSize = true;
+            this.deduccionAFP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deduccionAFP.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.deduccionAFP.Location = new System.Drawing.Point(3, 0);
+            this.deduccionAFP.Name = "deduccionAFP";
+            this.deduccionAFP.Size = new System.Drawing.Size(27, 20);
+            this.deduccionAFP.TabIndex = 22;
+            this.deduccionAFP.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(3, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 20);
+            this.label8.TabIndex = 20;
+            // 
+            // valorISR
+            // 
+            this.valorISR.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.valorISR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.valorISR.Controls.Add(this.deduccionISR);
+            this.valorISR.Controls.Add(this.label7);
+            this.valorISR.Location = new System.Drawing.Point(10, 72);
+            this.valorISR.Name = "valorISR";
+            this.valorISR.Size = new System.Drawing.Size(124, 20);
+            this.valorISR.TabIndex = 17;
+            // 
+            // deduccionISR
+            // 
+            this.deduccionISR.AutoSize = true;
+            this.deduccionISR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deduccionISR.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.deduccionISR.Location = new System.Drawing.Point(3, -1);
+            this.deduccionISR.Name = "deduccionISR";
+            this.deduccionISR.Size = new System.Drawing.Size(27, 20);
+            this.deduccionISR.TabIndex = 1;
+            this.deduccionISR.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(26, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Sueldo Neto";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.deduccionSFS);
+            this.panel4.Controls.Add(this.valorSFS);
+            this.panel4.Location = new System.Drawing.Point(324, 72);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(124, 20);
+            this.panel4.TabIndex = 19;
+            // 
+            // deduccionSFS
+            // 
+            this.deduccionSFS.AutoSize = true;
+            this.deduccionSFS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deduccionSFS.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.deduccionSFS.Location = new System.Drawing.Point(3, -1);
+            this.deduccionSFS.Name = "deduccionSFS";
+            this.deduccionSFS.Size = new System.Drawing.Size(27, 20);
+            this.deduccionSFS.TabIndex = 22;
+            this.deduccionSFS.Text = "00";
+            // 
+            // valorSFS
+            // 
+            this.valorSFS.AutoSize = true;
+            this.valorSFS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valorSFS.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.valorSFS.Location = new System.Drawing.Point(3, 0);
+            this.valorSFS.Name = "valorSFS";
+            this.valorSFS.Size = new System.Drawing.Size(0, 20);
+            this.valorSFS.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 372);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Sueldo);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.valorAFP.ResumeLayout(false);
+            this.valorAFP.PerformLayout();
+            this.valorISR.ResumeLayout(false);
+            this.valorISR.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Sueldo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel valorAFP;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel valorISR;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label valorSFS;
+        private System.Windows.Forms.Label totalSueldoNeto;
+        private System.Windows.Forms.Label deduccionAFP;
+        private System.Windows.Forms.Label deduccionISR;
+        private System.Windows.Forms.Label deduccionSFS;
+    }
+}
+
---
+++ WindowsFormAppSencilla1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormAppSencilla1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+
+        private void Calcular()
+        {
+            double sueldoBruto, sueldoNeto, ISR, AFP, SFS;
+
+            sueldoBruto = double.Parse(Sueldo.Text);
+
+            ISR = 0.09 * sueldoBruto;
+            AFP = 0.07 * sueldoBruto;
+            SFS = 0.1 * sueldoBruto;
+            sueldoNeto = sueldoBruto - (ISR + AFP + SFS);
+
+            deduccionISR.Text = ISR.ToString();
+            deduccionAFP.Text = AFP.ToString();
+            deduccionSFS.Text = SFS.ToString();
+            totalSueldoNeto.Text = sueldoNeto.ToString();
+
+        }
+    }
+}
